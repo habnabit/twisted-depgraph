8554e1776f64f142b0dcb8259a669807d57b2cf9
Aaron Gallagher <_@habnab.it>
2015-04-13 17:48:14 -0400

    Simplify travis config; update build script.
    Also don't show the GH_TOKEN environment variable in the travis log.

--- .build.sh
+++ .build.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-set -ex
-rev=$(git rev-parse HEAD)
+set -e
+git clone -b gh-pages "https://${GH_TOKEN}@github.com/habnabit/twisted-depgraph"
+set -x
+git clone https://github.com/twisted/twisted
+pip install -e twisted
+depgraph_rev=$(git rev-parse HEAD)
+twisted_rev=$(git -C twisted rev-parse HEAD)
 python twisted-depgraph.py twisted
 # fix this if you need it to be more flexible (I don't care right now)
-git clone -b gh-pages "https://${GH_TOKEN}@github.com/habnabit/twisted-depgraph"
 mv *.json twisted-depgraph
 cd twisted-depgraph
 git config user.name "${GIT_NAME}"
 git config user.email "${GIT_EMAIL}"
 git add *.json
-git commit -m "[travis-ci] Built from ${rev}."
+git commit -m "[travis-ci] Built from twisted ${twisted_rev}.
+
+twisted-depgraph was ${depgraph_rev}."
 git push origin gh-pages
