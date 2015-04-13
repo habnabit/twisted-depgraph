a5140241a355508d19f005f9fe74a4bb86063aef
Aaron Gallagher <_@habnab.it>
2015-04-13 17:19:19 -0400

    Add travis config stuff.

---
+++ .build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+rev=$(git rev-parse HEAD)
+python twisted-depgraph.py twisted
+# fix this if you need it to be more flexible (I don't care right now)
+git clone -b gh-pages "https://${GH_TOKEN}@github.com/habnabit/twisted-depgraph"
+mv *.json twisted-depgraph
+cd twisted-depgraph.py
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+git add *.json
+git commit -m "[travis-ci] Built from ${rev}."
+git push origin master
