787b0248933f6fb2a2c1995f11cbc0f52e158a76
Aaron Gallagher <_@habnab.it>
2015-04-13 19:24:05 -0400

    Forgot to move this comment.

--- .build.sh
+++ .build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+# fix this if you need it to be more flexible (I don't care right now)
 git clone -b gh-pages "https://${GH_TOKEN}@github.com/habnabit/twisted-depgraph"
 set -x
 git clone https://github.com/twisted/twisted
@@ -7,7 +8,6 @@ pip install -e twisted
 depgraph_rev=$(git rev-parse HEAD)
 twisted_rev=$(git -C twisted rev-parse HEAD)
 python twisted-depgraph.py twisted
-# fix this if you need it to be more flexible (I don't care right now)
 mv *.json twisted-depgraph
 cd twisted-depgraph
 git config user.name "${GIT_NAME}"
